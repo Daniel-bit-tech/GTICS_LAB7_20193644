de4e61712a673333078c35fcebee07ce5ce51eb1
daniel_2 <dani6468@gmail.com>
2025-10-23 19:25:51 -0500

    primer commit - base proyecto

---
+++ src/test/java/com/example/lab07_20202218/Lab0720202218ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab07_20202218;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab0720202218ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
